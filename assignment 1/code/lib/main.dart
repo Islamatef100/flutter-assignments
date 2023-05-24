b7ec69316831fbc8c1a41dfdca97c6a62d365201
islam atef nagy <78918678+Islamatef100@users.noreply.github.com>
2023-05-24 23:55:37 +0300

    Add files via upload

---
+++ assignment 1/code/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text('First Application'),
+        backgroundColor: Colors.green,
+      ),
+      body: const Center(
+        child: Text("Islam Atef Nagy"),
+      ),
+    ),
+  ));
+}
